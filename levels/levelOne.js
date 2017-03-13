b767fe252398c9a3166e2ed4a118e5cb9056e4c7
SENAC\francisco.cnmarao <francisco.cnmarao@sp.senac.br>
2017-03-13 10:10:17 -0300

    lvl1

--- levels/levelOne.js
+++ levels/levelOne.js
@@ -1,22 +1,10 @@
 var levelOne = {tiles:
-" MM     \n"+
-" MM     \n"+
-" MM   M \n"+
+" MM  MM \n"+
+" M    M \n"+
 "        \n"+
-" MMM    \n"+
-" M M  M \n"+
-" MMM  M \n"+
+"        \n"+
+"        \n"+
+" M    M \n"+
+" MM  MM \n"+
 "        ",
-    movingPlatforms: [{ id: "3-51", features: { moving: true }, settings: { movingOnX: true } }],
-    levelSwiches: [//{ id: "8-54", features: { moveTarget: true }, settings: { targetId: "6-58", defaulTimer: 120, direction: "U" } },
-                   { id: "8-55", features: { moveTarget: true }, settings: { targetId: "6-58", timed: true, defaulTimer: 120, direction: "D" } },
-                   { id: "8-56", features: { moveTarget: true }, settings: { targetId: "6-58", timed: true, defaulTimer: 120, direction: "L" } },
-                   //{ id: "8-57", features: { moveTarget: true }, settings: { targetId: "6-58", defaulTimer: 120, direction: "R" } }
-                   { id: "8-32", features: { moveTarget: true }, settings: { targetId: "8-33", timed: true, defaulTimer: 120, direction: "U", distance: 1 } },
-                   ],
-    customProperties: [{ id: "4-12", settings: { searchDepth: 8 } }],
-backGround:
-"X X \n" +
-" X X\n" +
-"X X \n" +
-" X X"}
+    customProperties: []}
