5629acb81f88f8e0174fdd7ba297e0cabb9d9161
SENAC\francisco.cnmarao <francisco.cnmarao@sp.senac.br>
2017-03-17 15:34:01 -0300

    lvl1

--- levels/levelOne.js
+++ levels/levelOne.js
@@ -1,11 +1,11 @@
 var levelOne = {tiles:
 "MMMMMMMM\n"+
-"MMMMMMMM\n"+
-"BBBMMMMM\n"+
-"BGBMMMMM\n"+
-"GBBMMMMM\n"+
-"MMMMMMMM\n"+
-"MMMMMMMM\n"+
+"MMM  MMM\n"+
+"MM    MM\n"+
+"M      M\n"+
+"M      M\n"+
+"MM    MM\n"+
+"MMM  MMM\n"+
 "MMMMMMMM",
     customProperties: []}
 /*
